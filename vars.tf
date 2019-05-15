4da8d5c33dae982095edad9ec972f5376d56b4b4
ashfakahmed2018 <38567677+ashfakahmed2018@users.noreply.github.com>
2019-05-15 00:17:31 -0400

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -2,10 +2,10 @@ variable "AWS_REGION" {
   default = "us-east-1"
 }
 variable "PATH_TO_PRIVATE_KEY" {
-  default = "mykey1"
+  default = "mykey"
 }
 variable "PATH_TO_PUBLIC_KEY" {
-  default = "mykey1.pub"
+  default = "mykey.pub"
 }
 variable "AMIS" {
   type = "map"
